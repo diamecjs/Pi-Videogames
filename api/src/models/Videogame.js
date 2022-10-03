74b6c7ac242fa060af9912733453cdc440f4a50d
diamecjs <diame.cjs@gmail.com>
2022-10-03 11:28:29 -0500

    finish

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,41 @@
+const { DataTypes, UUIDV4 } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    inDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
---
+++ api/src/routes/genres.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const axios = require("axios");
+const { Genres } = require('../db.js');
+const { API_KEY } = process.env;
+
+const router = Router();
+
+router.get ('/', async(req,res)=>{
+    try {
+    const genresApi = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`) 
+    const genresArray = await genresApi.data.results ;
+  
+    genresArray.forEach(genres =>{
+      Genres.findOrCreate({ where : { name : genres.name} })
+    })
+    const allGenres = await Genres.findAll();
+    res.send(allGenres)
+    } catch (error) {
+      console.log("error en ruta get/genres", error)
+    }
+  })
+
+module.exports = router;
